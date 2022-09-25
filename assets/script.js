cf6761a4590a78220aa459753e8aa026be1f5ff1
Sachi Takaoka <sachikaleikautakaoka@gmail.com>
2022-09-25 13:24:50 -0600

    js array

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,20 @@
 // Assignment Code
+
+// All 4 arrays needed for password generator
+
+
+// Lowercase Characters 
+var lowCaseChar = ["a", "b",];
+
+// Uppercase Characters
+var upperCaseChar = ["A", "B",];
+
+// Numeric Characters 0-9
+var numericChar = ["0", "1",];
+
+// Special Characters
+var specialChar = ["!", "@",];
+
 var generateBtn = document.querySelector(“#generate”);
 
 // Write password to the #password input
