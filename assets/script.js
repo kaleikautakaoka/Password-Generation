0ac296d05da273c2bb64035691da8c6817926274
Sachi Takaoka <sachikaleikautakaoka@gmail.com>
2022-09-25 21:50:21 -0600

    variableslegwork

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,16 @@
 
 
 // Lowercase Characters 
-var lowCaseChar = ["a", "b",];
+var lowCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Uppercase Characters
-var upperCaseChar = ["A", "B",];
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Numeric Characters 0-9
-var numericChar = ["0", "1",];
+var numericChar = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9"];
 
 // Special Characters
-var specialChar = ["!", "@",];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
 var generateBtn = document.querySelector(“#generate”);
 
