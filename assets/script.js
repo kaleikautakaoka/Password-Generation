e8b7809ae645846a5c0ee866d73f08bf430c20b4
Sachi Takaoka <sachikaleikautakaoka@gmail.com>
2022-10-14 23:30:02 -0600

    variables

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  console.log("click");
+  var userInput = window.prompt("Password length?")
+
+  var pwdLength = parseInt(userInput)
+
+  if (isNaN(pwdLength)) {
+    window.alert ("invalid")
+    return
+  }
+  if (pwdLength < 8 || pwdLength > 110) {
+    window.alert("Must be between 8 - 110 characters")
+    return
+  }
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
