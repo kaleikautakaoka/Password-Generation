eb2bdaf5dacb993fd9a7b5930ed364e057c6aedd
Sachi Takaoka <sachikaleikautakaoka@gmail.com>
2022-10-14 23:47:56 -0600

    more variables

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,37 @@ function generatePassword() {
   }
 }
 
+var wantNumbers = window.confirm("Include Numbers?")
+var specialChars = windows.confirm("Include Special Characters?")
+var lowerChars = windows.confirm("Include Lower Case Letters?")
+var UpperChars = windows.confirm("Include Upper Case Letters?")
 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "`", "-", "="]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var combinedVar = []
+
+for (var i = 0; i < lowerChars.length; i++) {
+  upperChars[i] = lowerChars[i].toUpperCase()
+}
+
+if (wantNumbers === true) {
+  combinedVar.push(numbers)
+}
+
+if (specialCharacters === true) {
+  combinedVar.push(symbols)
+}
+
+if (lowerChars === true) {
+  combinedVar.push(lowercase)
+}
+
+if (upperChars === true) {
+  combinedVar.push(uppercase)
+}
 
 // Write password to the #password input
 function writePassword() {
