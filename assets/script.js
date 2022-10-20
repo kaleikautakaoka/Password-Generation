8fcde6ad94debed79f44cc67b0fa409109d888a2
Sachi Takaoka <sachikaleikautakaoka@gmail.com>
2022-10-20 14:13:58 -0600

    variable differientiation

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ function randomInt(min, max) {
     max = min
     min = 0 
   }
-  var rdm = Math.random()
+  var rdm = Math.random();
   return Math.floor(min*(1-rdm) + rdm*max)
 }
 
@@ -33,35 +33,35 @@ function generatePwd() {
     return
    }
   //Prompts for user to select options they want in their password
-   var wantNumbers = windows.confirm("Include Numbers?")
-   var specialChar = windows.confirm("Include Special Characters?")
-   var lowerCase = windows.confirm("Include Lower Case Letters?")
-   var upperCase = windows.confirm("Include Upper Case Letters?")
+   var wantNumbers = window.confirm("Include Numbers?")
+   var specialChar = window.confirm("Include Special Characters?")
+   var lowerCase = window.confirm("Include Lower Case Letters?")
+   var upperCase = window.confirm("Include Upper Case Letters?")
 
    //arrays for character types
-   var wantNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-   var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "`", "-", "="]
-   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+   var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+   var symbolArray = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "`", "-", "="]
+   var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+   var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
    //array to combine all the variables 
-   var concatvar = []
+   var combinedVar = []
 
    //the next four "if" conditions added so that any given array option can be added based on user selection
 
 if (wantNumbers === true) {
-  concatvar.push(wantNumbers)
+  combinedVar.push(numberArray)
 }
 
 if (specialChar === true) {
-  concatvar.push(specialChar)
+  combinedVar.push(symbolArray)
 }
 
 if (lowerCase === true) {
-  concatvar.push(lowerCase)
+  combinedVar.push(lowerArray)
 }
 
 if (upperCase === true) {
-  concatvar.push(upperCase)
+  combinedVar.push(upperArray)
 }
 
 //if characters have not been selected this prompt/windowalert will appear
@@ -74,15 +74,16 @@ var generatedPwd = ""
 
 //random generation of variables
 for (var i = 0; i < pwdLength; i++) {
-  var rdmList = getRandomItem(concatvar)
-  var rdmChar = getRandomItem(rdmList)
+  var rdmList = getRandomItem(combinedVar);
+  var rdmChar = getRandomItem(rdmList);
   generatedPwd += rdmChar
 }
 return generatedPwd
+}
 
 // Display password to input
 function writePassword() {
-  var password = generatePwd(8, 110);
+  var pwd = generatePwd(8, 110);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = pwd;
